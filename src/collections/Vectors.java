be19b4a4f7603c20c28a5e8ec29f8e86853c2c84
Ujjwal <utamrakar3@gmail.com>
2025-02-27 19:35:08 +0530

    Collections introduction

---
+++ src/collections/Vectors.java
@@ -0,0 +1,14 @@
+package collections;
+
+import java.util.List;
+import java.util.Vector;
+
+public class Vectors {
+    public static void main(String[] args) {
+        List<Integer> vector=new Vector<>();
+        vector.add(1);
+        vector.add(2);
+        vector.add(3);
+        System.out.println(vector);
+    }
+}
